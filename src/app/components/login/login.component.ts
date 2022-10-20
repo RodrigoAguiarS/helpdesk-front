f2b7e809c98334f28583e80e97da8428826a9e7f
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2022-10-20 20:48:25 -0300

    adicionando auth de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { Credenciais } from 'src/app/models/credenciais';
@@ -18,14 +20,20 @@ export class LoginComponent implements OnInit {
   email = new FormControl(null, Validators.email);
   senha = new FormControl(null, Validators.minLength(3));
 
-  constructor(private toast: ToastrService) { }
+  constructor(private toast: ToastrService,
+    private service: AuthService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
   logar() {
-    this.toast.error('Usuário e/ou senha inválidos!', 'Login');
-    this.creds.senha = '';
+    this.service.authenticate(this.creds).subscribe(resposta => {
+      this.service.sucessFulLogin(resposta.headers.get('Authorization').substring(7));
+      this.router.navigate(['']);
+    }, () => {
+      this.toast.error('Usuário e/ou senha inválidas');
+    })
   }
 
   validaCampos(): boolean {
